858d88b519dfd85a3635307882df4d8596367efd
Shikhar <shikharg21@gmail.com>
2020-02-07 00:49:29 +0530

    UI Done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
+import React from'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+
+  render(){
+    return(
+      <div className='App-header'>
+        <img src={logo} width="100" height="100" className='App-logo'/>
+        <h1 className='app-title'>ToDo App</h1>
+        <div className='container'>
+          Add an item...
+          <br />
+          <input type="text" className="input-text" placeholder="Write a task" />
+          <button className='add-btn'>Add task</button>
+          <div className='list'>
+            <ul>
+              <li>
+                  <input type="checkbox" name=" "  />
+                  record
+                  <button className='btn'>Delete</button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
